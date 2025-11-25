f548c91fbd807ef0858b47c1cc2c5c97ddc13026
APAC\Jimmy-HY_Liu <jimmy-hy.liu@sgs.com>
2025-11-25 17:20:35 +0800

    Mainform UI介面初步

---
+++ RF_Schedule/MainForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace RF_Schedule
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            accordionControl1 = new DevExpress.XtraBars.Navigation.AccordionControl();
+            accordionControlElement1 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement2 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement3 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement4 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement5 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement6 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement7 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement8 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement9 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement10 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement11 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement12 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement13 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement14 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement15 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement16 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement17 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement18 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement19 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement20 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement21 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            ((System.ComponentModel.ISupportInitialize)panelControl1).BeginInit();
+            panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)accordionControl1).BeginInit();
+            SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            panelControl1.Controls.Add(accordionControl1);
+            panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            panelControl1.Location = new System.Drawing.Point(0, 0);
+            panelControl1.Name = "panelControl1";
+            panelControl1.Size = new System.Drawing.Size(1961, 934);
+            panelControl1.TabIndex = 0;
+            // 
+            // accordionControl1
+            // 
+            accordionControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            accordionControl1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement1 });
+            accordionControl1.Location = new System.Drawing.Point(2, 2);
+            accordionControl1.Name = "accordionControl1";
+            accordionControl1.Size = new System.Drawing.Size(390, 930);
+            accordionControl1.TabIndex = 0;
+            // 
+            // accordionControlElement1
+            // 
+            accordionControlElement1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement2, accordionControlElement3, accordionControlElement4 });
+            accordionControlElement1.Expanded = true;
+            accordionControlElement1.Name = "accordionControlElement1";
+            accordionControlElement1.Text = "主選單";
+            // 
+            // accordionControlElement2
+            // 
+            accordionControlElement2.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement5, accordionControlElement6, accordionControlElement7 });
+            accordionControlElement2.Expanded = true;
+            accordionControlElement2.Name = "accordionControlElement2";
+            accordionControlElement2.Text = "工程師區";
+            // 
+            // accordionControlElement3
+            // 
+            accordionControlElement3.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement8, accordionControlElement9, accordionControlElement10, accordionControlElement11, accordionControlElement12, accordionControlElement13, accordionControlElement14, accordionControlElement15 });
+            accordionControlElement3.Expanded = true;
+            accordionControlElement3.Name = "accordionControlElement3";
+            accordionControlElement3.Text = "主管區";
+            // 
+            // accordionControlElement4
+            // 
+            accordionControlElement4.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement16, accordionControlElement17, accordionControlElement18, accordionControlElement19, accordionControlElement20, accordionControlElement21 });
+            accordionControlElement4.Expanded = true;
+            accordionControlElement4.Name = "accordionControlElement4";
+            accordionControlElement4.Text = "Admin";
+            // 
+            // accordionControlElement5
+            // 
+            accordionControlElement5.Name = "accordionControlElement5";
+            accordionControlElement5.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement5.Text = "我的工作";
+            // 
+            // accordionControlElement6
+            // 
+            accordionControlElement6.Name = "accordionControlElement6";
+            accordionControlElement6.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement6.Text = "工時紀錄";
+            // 
+            // accordionControlElement7
+            // 
+            accordionControlElement7.Name = "accordionControlElement7";
+            accordionControlElement7.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement7.Text = "Loading分析";
+            // 
+            // accordionControlElement8
+            // 
+            accordionControlElement8.Name = "accordionControlElement8";
+            accordionControlElement8.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement8.Text = "案件管理";
+            // 
+            // accordionControlElement9
+            // 
+            accordionControlElement9.Name = "accordionControlElement9";
+            accordionControlElement9.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement9.Text = "法規管理";
+            // 
+            // accordionControlElement10
+            // 
+            accordionControlElement10.Name = "accordionControlElement10";
+            accordionControlElement10.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement10.Text = "測項管理";
+            // 
+            // accordionControlElement11
+            // 
+            accordionControlElement11.Name = "accordionControlElement11";
+            accordionControlElement11.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement11.Text = "工程師指派";
+            // 
+            // accordionControlElement12
+            // 
+            accordionControlElement12.Name = "accordionControlElement12";
+            accordionControlElement12.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement12.Text = "補測版本管理";
+            // 
+            // accordionControlElement13
+            // 
+            accordionControlElement13.Name = "accordionControlElement13";
+            accordionControlElement13.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement13.Text = "工時審核";
+            // 
+            // accordionControlElement14
+            // 
+            accordionControlElement14.Name = "accordionControlElement14";
+            accordionControlElement14.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement14.Text = "Loading分析";
+            // 
+            // accordionControlElement15
+            // 
+            accordionControlElement15.Name = "accordionControlElement15";
+            accordionControlElement15.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement15.Text = "延遲分析";
+            // 
+            // accordionControlElement16
+            // 
+            accordionControlElement16.Name = "accordionControlElement16";
+            accordionControlElement16.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement16.Text = "使用者管理";
+            // 
+            // accordionControlElement17
+            // 
+            accordionControlElement17.Name = "accordionControlElement17";
+            accordionControlElement17.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement17.Text = "權限管理";
+            // 
+            // accordionControlElement18
+            // 
+            accordionControlElement18.Name = "accordionControlElement18";
+            accordionControlElement18.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement18.Text = "群組管理";
+            // 
+            // accordionControlElement19
+            // 
+            accordionControlElement19.Name = "accordionControlElement19";
+            accordionControlElement19.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement19.Text = "延遲原因管理";
+            // 
+            // accordionControlElement20
+            // 
+            accordionControlElement20.Name = "accordionControlElement20";
+            accordionControlElement20.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement20.Text = "系統設定";
+            // 
+            // accordionControlElement21
+            // 
+            accordionControlElement21.Name = "accordionControlElement21";
+            accordionControlElement21.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement21.Text = "稽核日誌";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1961, 934);
+            Controls.Add(panelControl1);
+            Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            Name = "MainForm";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)panelControl1).EndInit();
+            panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)accordionControl1).EndInit();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraBars.Navigation.AccordionControl accordionControl1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement2;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement3;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement4;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement5;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement6;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement7;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement8;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement9;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement10;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement11;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement12;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement13;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement14;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement15;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement16;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement17;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement18;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement19;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement20;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement21;
+    }
+}
+
